fe01f9bce93afbfe44c21be929e2fc351851b9fd
aLai502 <104610544+aLai502@users.noreply.github.com>
2022-12-02 23:52:44 +0800

    Layout update

--- app/src/main/java/com/iacademy/e_wallet/activities/CashInActivity.java
+++ app/src/main/java/com/iacademy/e_wallet/activities/CashInActivity.java
@@ -3,14 +3,25 @@ package com.iacademy.e_wallet.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.iacademy.e_wallet.R;
 
 public class CashInActivity extends AppCompatActivity {
 
+    private EditText etAmount;
+    private TextView tvCurrentBalance;
+    private Button bConfirmCash;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash_in);
+
+        etAmount = findViewById(R.id.etAmount);
+        tvCurrentBalance = findViewById(R.id.tvCurrentBalance);
+        bConfirmCash = findViewById(R.id.bConfirmCash);
     }
 }
--- app/src/main/java/com/iacademy/e_wallet/activities/SendMoneyActivity.java
+++ app/src/main/java/com/iacademy/e_wallet/activities/SendMoneyActivity.java
@@ -3,14 +3,26 @@ package com.iacademy.e_wallet.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.iacademy.e_wallet.R;
 
 public class SendMoneyActivity extends AppCompatActivity {
 
+    private EditText etNumber, etAmount;
+    private TextView tvAvailBalance;
+    private Button bSendMoney;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_money);
+
+        etNumber = findViewById(R.id.etNumber);
+        etAmount = findViewById(R.id.etAmount);
+        tvAvailBalance = findViewById(R.id.tvAvailBalance);
+        bSendMoney = findViewById(R.id.bSendMoney);
     }
 }
